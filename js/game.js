7336910423010bee85dd6fc0cb5dde0111c35898
amir <amirrezazadeh.job@gmail.com>
2026-02-17 14:34:57 +0330

    change default photos

--- js/game.js
+++ js/game.js
@@ -14,7 +14,7 @@ async function getHeader() {
     if (!res.ok) showVpnModal();
     let response = await res.json();
     document.querySelector("#header-content").innerHTML += `
-        <div class="w-full h-full bg-cover " style="background-image: url('${response.background_image}') , url('img/search-page-background.png');">
+        <div class="w-full h-full bg-cover " style="background-image: url('${response.background_image}') , url('img/default-bg.jpg');">
             <div class="w-full h-full relative flex items-center bg-gradient-to-l from-black/80 to-transparent">
             <div  class="text-white mt-21 my-5 md:mb-10  flex flex-col items-start gap-4 sm:gap-5 lg:gap-8  pr-5 md:pr-10 w-full xl:w-2/3  lg:items-start lg:pr-20">
               <div class="w-full flex ">
--- js/index.js
+++ js/index.js
@@ -16,8 +16,8 @@ async function getHeader() {
     onTop.forEach((elem) => {
       document.querySelector("#header-swiper-wrapper").innerHTML += `
           <div class="swiper-slide">
-              <div class=" w-screen h-[80vh] md:h-screen shrink-0  bg-cover " style=" background-image: url('${elem.background_image}') , url('img/search-page-background.png')" >
-              <div class="w-full h-full relative flex flex-col justify-center items-center lg:items-start lg:pr-25 bg-gradient-to-l from-black/80 to-transparent">
+              <div class=" w-screen h-full md:h-screen shrink-0  bg-cover " style=" background-image: url(${elem.background_image}) , url('img/default-bg.jpg')" >
+              <div class="w-full h-full relative flex flex-col justify-center items-center lg:items-start lg:pr-25 bg-gradient-to-l from-black/80 to-transparent to-45%">
               
                 <div class="animated absolute w-1/1 h-auto z-30 px-5 sm:w-3/4 sm:px-0 lg:w-2/4">
                   <div class="inline-flex items-center gap-3 w-auto  border-1 px-3 py-1 border-gray-300 rounded-full ">
